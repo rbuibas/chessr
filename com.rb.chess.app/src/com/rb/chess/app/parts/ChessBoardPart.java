8964813dcfdfaa2a70a78c0ba5031c87235a5b84
Raul Buibas <buibas.raul@gmail.com>
2019-01-18 11:52:03 +0100

    add row numbers and letters for the board

--- com.rb.chess.app/src/com/rb/chess/app/parts/ChessBoardPart.java
+++ com.rb.chess.app/src/com/rb/chess/app/parts/ChessBoardPart.java
@@ -14,12 +14,14 @@ import com.rb.chess.room.ChessRoom;
 
 public class ChessBoardPart {
 	
+	private static final String[] COL_LETTER = {"A", "B", "C", "D", "E", "F", "G", "H"};
 	private ChessRoom chessRoom;
 	private Label[][] squares;
 	
 	public ChessBoardPart() {
 		chessRoom = new ChessRoom();
-		squares = new Label[Board.LENGTH][Board.LENGTH];
+		// +1 is for numbers and letters
+		squares = new Label[Board.LENGTH + 1][Board.LENGTH + 1];
 	}
 
 	@PostConstruct
@@ -30,22 +32,37 @@ public class ChessBoardPart {
 		// but first set the layout 
 		// GridLayout lets you organize your widget as a matrix
 		
-		parent.setLayout(new GridLayout(Board.LENGTH, true));
+		// +1 is for numbers and letters		
+		parent.setLayout(new GridLayout(Board.LENGTH + 1, false));
 		
 		// arrange a little bit the data represented
 		// the gridData object will center horizontally and vertically
 		// it's a good practice to center 
-		GridData gridData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
+		GridData squareGridData = new GridData(SWT.FILL, SWT.CENTER, false, false);
+		// another grid data for tags (letters and numbers on the side of the board)
+		GridData tagGridData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
 		for (int r = 0; r < Board.LENGTH; r++) {
+			// add the numbers to the board
+			squares[r][0] = new Label(parent, SWT.NONE);
+			squares[r][0].setLayoutData(tagGridData);
+			squares[r][0].setText(Integer.toString(r + 1));
 			for (int c = 0; c < Board.LENGTH; c++) {
 				squares[r][c] = new Label(parent, SWT.BORDER);
 				// center it a little bit
-				squares[r][c].setLayoutData(gridData);
+				squares[r][c].setLayoutData(squareGridData);
 				// with set data we can link any object to the widget
 				// in this case we link our Square object
 				squares[r][c].setData(chessRoom.getBoard().getSquare(r, c));
-				squares[r][c].setText(squares[r][c].getData().toString());
+				squares[r][c].setText(squares[r][c].getData().toString() + "XXXXX");
 			}
+		}
+		squares[Board.LENGTH][0] = new Label(parent, SWT.NONE);
+		squares[Board.LENGTH][0].setLayoutData(tagGridData);
+		for (int i = 0; i < Board.LENGTH; i++) {
+			squares[Board.LENGTH][i + 1] = new Label(parent, SWT.NONE);
+			squares[Board.LENGTH][i + 1].setLayoutData(tagGridData);
+//			squares[Board.LENGTH][i + 1].setText(Character.toString((char) ('A' + i)));
+			squares[Board.LENGTH][i + 1].setText(COL_LETTER[i]);
 		}
 	}
 
